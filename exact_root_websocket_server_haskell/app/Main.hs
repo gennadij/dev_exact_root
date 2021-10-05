d08e4e9e3611c5e5392b4db133a01d6d6abd3cd7
Gennadi <gennadiheimann@gmail.com>
2021-10-05 22:54:51 +0200

    haskell json parser funktioniert

--- exact_root_websocket_server_haskell/app/Main.hs
+++ exact_root_websocket_server_haskell/app/Main.hs
@@ -6,16 +6,18 @@ import Data.Char (isPunctuation, isSpace, isDigit)
 import Data.Monoid (mappend)
 import Data.Text (Text)
 import Control.Exception (finally)
-import Control.Monad (forM_, forever)
+import Control.Monad (forM_, forever, mzero)
 import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
 
-import qualified ExactRoot as ER ( berechneWurzel, Ergebnis ( .. )) 
+import qualified ExactRoot as ER ( berechneWurzel, Ergebnis ( .. ))
+import qualified ExactRoot2 as ER2 ( berechneExacteWurzel, Res ( .. )) 
 import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy as LB
+import GHC.Base (MonadPlus(mzero))
 
 -- new TestTyp
 type Clients = [WS.Connection]
@@ -45,13 +47,63 @@ data ResponseJson = ResponseJson {
   responseData :: JsonData
 } deriving (Show, Generic, FromJSON, ToJSON)
 
--- instance FromJSON ExactRoot
--- instance ToJSON ExactRoot
+-- Client request `{"jsonrpc":"2.0","method":"exact_root","params":{"radikand":50},"id":1}`
 
+-- Server respons `{"jsonrpc":"2.0","result":{"multiplikator":5,"wurzelwert":2},"id":1}`
+data Response = Response {
+  response_jsonrpc :: String,
+  response_result :: Result',
+  response_id :: Int
+} deriving (Show)
+
+data Result' = Result' {
+  result_multiplikator :: Int,
+  result_wurzelwert :: Int,
+  result_radikand :: Int
+} deriving (Show)
+
+data Request = Request {
+  request_jsonrpc :: String,
+  request_method :: String,
+  request_params :: Params,
+  request_id :: Int
+} deriving (Show)
+
+newtype Params = Params {
+  params_radikand :: Int
+} deriving (Show)
+
+instance FromJSON Request where
+ parseJSON (Object v) =
+    Request <$> v .: "jsonrpc"
+           <*> v .: "method"
+           <*> v .: "params"
+           <*> v .: "id"
+ parseJSON _ = mzero
+
+instance FromJSON Params where 
+  parseJSON (Object v) = 
+    Params <$> v .: "radikand"
+  parseJSON _ = mzero
+
+instance ToJSON Response where
+ toJSON (Response response_jsonrpc response_result response_id) =
+    object [ "jsonrpc"  .= response_jsonrpc
+           , "result"   .= response_result
+           , "id"       .= response_id
+             ]
+
+instance ToJSON Result' where
+  toJSON (Result' result_multiplikator result_wurzelwert result_radikand) = 
+    object [ "multiplikator" .= result_multiplikator
+           , "wurzelwert" .= result_wurzelwert
+           , "radikand" .= result_radikand
+    ]
 main :: IO ()
 main = do
 --    state <- newMVar newServerState
     T.putStrLn "Server started."
+    
     clients <- newMVar newClients
     WS.runServer "127.0.0.1" 9160 $ application clients
 
@@ -72,7 +124,12 @@ application clients pending = do
 talk :: WS.Connection -> MVar Clients -> IO ()
 talk conn clients = forever $ do
   msg <- WS.receiveData conn
-  -- print (decodeJson msg)
+  let response = Response {
+    response_jsonrpc = "2.0",
+    response_result = Result' 1 2 3, 
+    response_id = 1
+  }
+  print (show (encode response))
   print msg
   handleMsg (decodeJson msg) conn
 
